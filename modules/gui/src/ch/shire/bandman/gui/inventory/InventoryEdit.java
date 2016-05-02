48b8b82c3542b5c1ea3d08c9756d848634619595
Patrick Balsiger <frupii@gmail.com>
2016-05-02 14:25:11 +0000

    asset edit in overlay

--- modules/gui/src/ch/shire/bandman/gui/inventory/InventoryEdit.java
+++ modules/gui/src/ch/shire/bandman/gui/inventory/InventoryEdit.java
@@ -5,9 +5,27 @@ package ch.shire.bandman.gui.inventory;
 
 import com.haulmont.cuba.gui.components.AbstractWindow;
 import com.haulmont.cuba.gui.components.AbstractEditor;
+import com.haulmont.cuba.gui.WindowManager;
+import com.haulmont.cuba.gui.components.actions.CreateAction;
+import com.haulmont.cuba.gui.components.actions.EditAction;
+
+import javax.inject.Named;
+import java.util.Map;
 
 /**
  * @author user
  */
 public class InventoryEdit extends AbstractEditor {
+        
+    @Named("assetTable.create")
+    protected CreateAction assetTableCreate;
+    @Named("assetTable.edit")
+    protected EditAction assetTableEdit;
+    
+    @Override
+    public void init(Map<String, Object> params) {
+        assetTableCreate.setOpenType(WindowManager.OpenType.DIALOG);
+        assetTableEdit.setOpenType(WindowManager.OpenType.DIALOG);
+    }
+
 }
--- modules/gui/src/ch/shire/bandman/gui/inventory/Inventorylookup.java
+++ modules/gui/src/ch/shire/bandman/gui/inventory/Inventorylookup.java
@@ -3,15 +3,32 @@
  */
 package ch.shire.bandman.gui.inventory;
 
+
 import com.haulmont.cuba.gui.components.AbstractWindow;
 import com.haulmont.cuba.gui.components.AbstractLookup;
 import com.haulmont.cuba.gui.components.Component;
+import com.haulmont.cuba.gui.WindowManager;
+import com.haulmont.cuba.gui.components.actions.CreateAction;
+import com.haulmont.cuba.gui.components.actions.EditAction;
+
+import javax.inject.Named;
+import java.util.Map;
 
 /**
  * @author user
  */
 public class Inventorylookup extends AbstractLookup {
-
-    public void onCreate(Component source) {
+    /*
+    @Named("inventoryTable.create")
+    protected CreateAction inventoryTableCreate;
+    @Named("inventoryTable.edit")
+    protected EditAction inventoryTableEdit;
+    
+    @Override
+    public void init(Map<String, Object> params) {
+        inventoryTableCreate.setOpenType(WindowManager.OpenType.DIALOG);
+        inventoryTableEdit.setOpenType(WindowManager.OpenType.DIALOG);
     }
+    assetTable.create
+*/
 }
