3386932481a90146b91b9e49fc844f3e3e81e114
Patrick Balsiger <frupii@gmail.com>
2016-05-03 21:16:51 +0000

    reports added

--- modules/web/src/ch/shire/bandman/web/App.java
+++ modules/web/src/ch/shire/bandman/web/App.java
@@ -1,6 +1,11 @@
 package ch.shire.bandman.web;
 
 import com.haulmont.cuba.web.DefaultApp;
+import com.haulmont.charts.web.gui.ChartComponentPalette;
+import com.haulmont.cuba.web.gui.WebUIPaletteManager;
 
 public class App extends DefaultApp {
+    static {
+        WebUIPaletteManager.registerPalettes(new ChartComponentPalette());
+    }
 }
